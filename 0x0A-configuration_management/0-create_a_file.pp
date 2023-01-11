80ee7c00d711873cce9f1b82f53b7089d6dba6bc
MaureenMOguche <career.oguchemaureen@outlook.com>
2023-01-11 14:36:05 +0100

    task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,5 +1,6 @@
-file {
-  path => '/tmp/school':
+file { 'resource title':
+  path => '/tmp/school',
+  mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
   content => 'I love Puppet'
